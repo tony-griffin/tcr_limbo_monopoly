b8a54c4cdb959b5eec11eb382e24efa9923a6c82
John Nicholas <john.nicholas@madetech.com>
2020-05-13 16:39:55 +0100

    passing tests

---
+++ HaveFun/BankPropertySpec.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace HaveFun
+{
+    public class BankPropertySpec
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ HaveFun/MovementSpec.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace HaveFun
+{
+    public class MovementSpec
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
