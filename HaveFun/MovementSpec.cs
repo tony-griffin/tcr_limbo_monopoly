091ef48029411cbbd12b5f6ca85408e63a94ca28
John Nicholas <john.nicholas@madetech.com>
2020-05-15 14:41:48 +0100

    automatically commit passing test

--- HaveFun/MovementSpec.cs
+++ HaveFun/MovementSpec.cs
@@ -9,14 +9,41 @@ namespace HaveFun
         public void Test1()
         {
             var game = new Game();
-            var player1Wins = game.Handle(new StartGame());
-            Assert.Equal(typeof(PlayerOneWins), player1Wins.GetType());
+            var result = game.Handle(new StartGame());
+            Assert.Equal(typeof(GameStarted), result.GetType());
         }
+        
+        [Fact]
+        public void Test2()
+        {
+            var game = new Game();
+            var result = game.Handle(new MovePlayerOne());
+            Assert.Equal(typeof(PlayerOneWins), result.GetType());
+        }
+
+        [Fact]
+        public void asdf()
+        {
+            
+        }
+    }
+
+    public class MovePlayerOne : Command
+    {
+    }
+
+    public class GameStarted : Event
+    {
     }
 
     public class Game
     {
         public Event Handle(StartGame startGame)
+        {
+            return new GameStarted();
+        }
+
+        public Event Handle(MovePlayerOne cmd)
         {
             return new PlayerOneWins();
         }
@@ -30,7 +57,11 @@ namespace HaveFun
     {
     }
 
-    public class StartGame
+    public class StartGame : Command
+    {
+    }
+
+    public class Command
     {
     }
 }
